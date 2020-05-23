9fa36a33aff79a5768fe8be898da719ea6839a77
ibebbs <github@bebbs.co.uk>
2020-05-23 17:05:09 +0100

    Implemented workaround for azurite issue shown here: https://github.com/Azure/Azurite/issues/379

--- src/Beholder/Face/Recognizer/Implementation.cs
+++ src/Beholder/Face/Recognizer/Implementation.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.ML;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ML;
 using System.Linq;
 
 namespace Beholder.Face.Recognizer
@@ -6,19 +7,28 @@ namespace Beholder.Face.Recognizer
     public class Implementation : IRecognizer
     {
         private readonly PredictionEnginePool<InputModel, OutputModel> _predictionEnginePool;
+        private readonly ILogger<Implementation> _logger;
 
-        public Implementation(PredictionEnginePool<InputModel, OutputModel> predictionEnginePool)
+        public Implementation(PredictionEnginePool<InputModel, OutputModel> predictionEnginePool, ILogger<Implementation> logger)
         {
             _predictionEnginePool = predictionEnginePool;
+            _logger = logger;
         }
 
         public IRecognition RecogniseFace(IImage image)
         {
+            _logger.LogInformation("Recognising Face");
+
             var inputModel = new InputModel { Image = image.Data };
 
             var outputModel = _predictionEnginePool.Predict(inputModel);
 
-            return new Recognition(image.Data, new[] { new Tag(outputModel.PredictedLabel, outputModel.Score.Max()) });
+            var label = outputModel.PredictedLabel;
+            var score = outputModel.Score.Max();
+
+            _logger.LogInformation("Recognition complete. Identified as {0} with confidence {1:P2}", label, score);
+
+            return new Recognition(image.Data, new[] { new Tag(label, score) });
         }
     }
 }
