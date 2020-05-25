52e8231c3f6ed745b5e32a7478110b9a8cbe934a
ibebbs <github@bebbs.co.uk>
2020-05-25 15:09:07 +0100

    Reverted to using ASPNETCORE_URLS for adapter/port binding

--- src/Director/Program.cs
+++ src/Director/Program.cs
@@ -19,12 +19,9 @@ namespace Director
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureHostConfiguration(configuration => configuration.AddEnvironmentVariables("Director:"))
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureHostConfiguration(configuration => configuration.AddEnvironmentVariables("Director:"))
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
